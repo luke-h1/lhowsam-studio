91d9213b7a0e521f2c691c7cdd885b9b6ba1c87f
luke-h1 <luke.howsam@yahoo.com>
2022-12-31 16:26:53 +0000

    chore - re add prettierrc

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'wdj6ou2i',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,6 @@
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
-import { markdownSchema } from 'sanity-plugin-markdown'
+import {markdownSchema} from 'sanity-plugin-markdown'
 import {deskTool} from 'sanity/desk'
 import {schemaTypes} from './schemas'
 
