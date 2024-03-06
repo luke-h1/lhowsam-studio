f1837b03ee14f9c6f81d361b694b03f5f4b833b4
luke-h1 <luke.howsam@yahoo.com>
2024-03-06 21:13:09 +0000

    feat(studio): add draft capabilities

--- src/defaultDocumentNode.ts
+++ src/defaultDocumentNode.ts
@@ -1,31 +1,31 @@
-import {DefaultDocumentNodeResolver} from 'sanity/desk'
-import Iframe from 'sanity-plugin-iframe-pane'
+// import {DefaultDocumentNodeResolver} from 'sanity/desk'
+// import Iframe from 'sanity-plugin-iframe-pane'
 
-export const defaultDocumentNode: DefaultDocumentNodeResolver = (S, {schemaType}) => {
-  switch (schemaType) {
-    case 'post': {
-      return S.document().views([
-        S.view.form(),
-        S.view
-          .component(Iframe)
-          .options({
-            url: 'https://lhowsam.com/api/preview',
-          })
-          .title('Preview'),
-      ])
-    }
-    case 'project': {
-      return S.document().views([
-        S.view.form(),
-        S.view
-          .component(Iframe)
-          .options({
-            url: 'https://lhowsam.com/api/preview',
-          })
-          .title('Preview'),
-      ])
-    }
-    default:
-      return S.document().views([S.view.form()])
-  }
-}
+// export const defaultDocumentNode: DefaultDocumentNodeResolver = (S, {schemaType}) => {
+//   switch (schemaType) {
+//     case 'post': {
+//       return S.document().views([
+//         S.view.form(),
+//         S.view
+//           .component(Iframe)
+//           .options({
+//             url: 'https://lhowsam.com/api/preview',
+//           })
+//           .title('Preview'),
+//       ])
+//     }
+//     case 'project': {
+//       return S.document().views([
+//         S.view.form(),
+//         S.view
+//           .component(Iframe)
+//           .options({
+//             url: 'https://lhowsam.com/api/preview',
+//           })
+//           .title('Preview'),
+//       ])
+//     }
+//     default:
+//       return S.document().views([S.view.form()])
+//   }
+// }
